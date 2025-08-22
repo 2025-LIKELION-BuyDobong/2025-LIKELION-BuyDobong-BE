05c95480ba752d4665afebd76d77bd4c004f7da9
HeejuKo <khjg00gle96@gmail.com>
2025-08-22 18:11:40 +0900

    📝 Docs: 상점/상품 검색 및 조회 API Swagger 문서 수정
    Docs: 상점/상품 검색 및 조회 API Swagger 문서 수정

--- src/main/java/com/dobongsoon/BuyDobong/domain/consumer/search/controller/SearchController.java
+++ src/main/java/com/dobongsoon/BuyDobong/domain/consumer/search/controller/SearchController.java
@@ -26,15 +26,19 @@ public class SearchController {
      *     /api/consumer/1/search?query=케이크&markets=SINDOBONG&markets=BAEGUN
      */
     @Operation(
-            summary = "상품 검색",
+            summary = "상점/상품 검색 및 조회",
             description = """
-            특정 소비자가 키워드로 상품을 검색합니다.
+            특정 소비자가 키워드로 상점/상품을 검색하고 조회합니다.
             - 인증 필요: CONSUMER
             - 요청 파라미터:
               - query: 검색어 (필수)
               - markets: 시장 이름 목록 (선택, 0~n개 가능)
               - onlyDeal: 특가 상품만 조회 여부 (기본값: false)
-            - 응답: 상품 리스트 (id, name, price, market, deal 여부 등)
+            - 응답: 상점/상품 리스트 (id, name, price, market, deal 여부 등)
+            
+            ✅ interested: 관심 키워드 여부
+            -> 상점마다 중복으로 나타나는데... 하나만 나타나게 하기엔 조금 복잡해서 그냥 뒀어요
+               중복으로 계속 뜰 뿐, 여부는 정상 작동하니까 잘 갖다 쓰시면 될 것 같습니당 ^_^;;
             """
     )
     @GetMapping
