3cb6808f309a323f68160b5f2d85ad1310bd2cad
HeewonJo <heewonjo.me@gmail.com>
2025-08-22 21:32:07 +0900

    ✨ Feat: 이미지 응답 형식 변경

---
+++ src/main/java/com/dobongsoon/BuyDobong/domain/store/dto/StoreImageUploadResponse.java
@@ -0,0 +1,10 @@
+package com.dobongsoon.BuyDobong.domain.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StoreImageUploadResponse {
+    private String url;
+}
