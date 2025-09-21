1def797c4c8ed35c317cf54d22c28dfddd356b75
HeewonJo <heewonjo.me@gmail.com>
2025-09-22 04:53:46 +0900

    ✨ Feat: 회원 탈퇴 기능 구현

--- src/main/java/com/dobongsoon/BuyDobong/domain/auth/controller/AuthController.java
+++ src/main/java/com/dobongsoon/BuyDobong/domain/auth/controller/AuthController.java
@@ -5,14 +5,11 @@ import com.dobongsoon.BuyDobong.domain.auth.dto.LoginRequest;
 import com.dobongsoon.BuyDobong.domain.auth.dto.RegisterRequest;
 import com.dobongsoon.BuyDobong.domain.auth.service.AuthService;
 import com.dobongsoon.BuyDobong.domain.auth.service.LogoutService;
+import com.dobongsoon.BuyDobong.domain.auth.service.WithdrawService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     private final LogoutService logoutService;
+    private final WithdrawService withdrawService;
 
     @PostMapping("/register")
     @Operation(summary = "회원가입")
@@ -38,6 +36,14 @@ public class AuthController {
     public ResponseEntity<Void> logout(
             @RequestHeader(name = "Authorization", required = false) String authorizationHeader) {
         logoutService.logout(authorizationHeader);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/withdraw")
+    @Operation(summary = "회원 탈퇴")
+    public ResponseEntity<Void> withdraw(
+            @RequestHeader(name = "Authorization", required = false) String authroizationHeader) {
+        withdrawService.withdraw(authroizationHeader);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/dobongsoon/BuyDobong/domain/auth/service/WithdrawService.java
@@ -0,0 +1,5 @@
+package com.dobongsoon.BuyDobong.domain.auth.service;
+
+public interface WithdrawService {
+    void withdraw(String authrozationHeader);
+}
